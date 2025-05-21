9a6c238275cdfbc559b4a4d23ca2ceaf6fab778a
mafyyu <morita0126m@gmail.com>
2025-05-22 00:43:42 +0900

    デプロイ周りの設定

---
+++ deploy-commands.js
@@ -0,0 +1,24 @@
+import { REST, Routes } from 'discord.js';
+import heyFile  from './commands/hey.js'
+import config from './config.json' assert {type:'json'}
+
+const { applicationId, guidId, token } = config;
+
+// 登録コマンドを呼び出してリスト形式で登録
+const commands = [heyFile.data.toJSON()];
+
+// DiscordのAPIには現在最新のversion10を指定
+const rest = new REST({ version: '10' }).setToken(token);
+
+// Discordサーバーにコマンドを登録
+(async () => {
+    try {
+        await rest.put(
+			Routes.applicationGuildCommands(applicationId, config.guidId),
+			{ body: commands },
+		);
+        console.log('サーバー固有のコマンドが登録されました！');
+    } catch (error) {
+        console.error('コマンドの登録中にエラーが発生しました:', error);
+    }
+})();
