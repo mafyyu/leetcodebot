f6ccd6d5ab446b5065d7f1f5455a4abe10c2b51b
mafyyu <morita0126m@gmail.com>
2025-05-22 00:42:09 +0900

    関数をECM形式に変更

--- graphql/todayQuestion.js
+++ graphql/todayQuestion.js
@@ -1,30 +1,25 @@
-const query =`
-    query questionOfToday {
-    activeDailyCodingChallengeQuestion {
-        question {
-        questionId
-        title
-        titleSlug
-        difficulty
-        }
-    }
-    }
-`;
-
-
-fetch('https://leetcode.com/graphql', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',//JSONデータ送る合図みたいなやつ
-    'Referer': 'https://leetcode.com', // あると安全
-    'User-Agent': 'Mozilla/5.0',       // あると安全
-  },
-  body: JSON.stringify({
-    query
-  })
-})
-  .then(res => res.json())
-  .then(data => {
-    console.log(JSON.stringify(data, null, 2));
-  })
-  .catch(e => console.error(e));
+export const todayQuestion = async()=>{
+  const query =`
+      query questionOfToday {
+      activeDailyCodingChallengeQuestion {
+          question {
+          questionId
+          title
+          titleSlug
+          difficulty
+          }
+      }
+      }
+  `;
+  const res = await fetch('https://leetcode.com/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',//JSONデータ送る合図みたいなやつ
+      'Referer': 'https://leetcode.com', // あると安全
+      'User-Agent': 'Mozilla/5.0',       // あると安全
+    },
+    body: JSON.stringify({query})
+  });
+  const data = await res.json();
+  return JSON.stringify(data.data.activeDailyCodingChallengeQuestion.question.title)
+}
